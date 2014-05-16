827bd186c2038a5f794a21ddd21f422edb5d9e28
alexunit <alexunit@gmail.com>
2014-05-16 17:54:31 +0300

    initial upload

---
+++ app/models/Tag.php
@@ -0,0 +1,14 @@
+<?php
+
+class Tag extends Eloquent {
+	protected $guarded = array();
+
+   public static $rules = array(
+      'title' => 'required|min:2|max:200|unique:tags,title'
+   );
+
+   public function offers()
+   {
+      return $this->belongsToMany('Offer');
+   }
+}
